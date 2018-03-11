80bfcd70f0fcbbc1a681ac03bf00c314537a130d
alejandra-gonzalez <gonzalez.alejandra.013@gmail.com>
2018-03-10 22:19:17 -0800

    Lesson 7 Complete

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -23,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view){
-        displayPrice(quantity* 5);
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price + "\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -40,6 +42,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+    * This method displays the given text on the screen.
+    */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
